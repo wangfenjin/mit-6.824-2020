51363707882a667fc34001000b895f362fec3dce
wangfenjin <wangfenj@gmail.com>
2020-01-10 11:32:20 +0800

    pass shardmaster
    Change-Id: I6f75889d9170095f5fa624b378c2a6afe9f81a89

--- src/shardmaster/client.go
+++ src/shardmaster/client.go
@@ -12,6 +12,9 @@ import "math/big"
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// Your data here.
+	n      int
+	leader int
+	id     int64
 }
 
 func nrand() int64 {
@@ -25,6 +28,8 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// Your code here.
+	ck.n = len(servers)
+	ck.id = nrand()
 	return ck
 }
 
@@ -32,13 +37,17 @@ func (ck *Clerk) Query(num int) Config {
 	args := &QueryArgs{}
 	// Your code here.
 	args.Num = num
+	index := ck.leader
 	for {
 		// try each known server.
-		for _, srv := range ck.servers {
+		for i := 0; i < ck.n; i++ {
 			var reply QueryReply
-			ok := srv.Call("ShardMaster.Query", args, &reply)
-			if ok && reply.WrongLeader == false {
+			ok := ck.servers[index].Call("ShardMaster.Query", args, &reply)
+			if ok && !reply.WrongLeader && reply.Err == OK {
+				ck.leader = index
 				return reply.Config
+			} else {
+				index = (index + 1) % ck.n
 			}
 		}
 		time.Sleep(100 * time.Millisecond)
@@ -46,17 +55,22 @@ func (ck *Clerk) Query(num int) Config {
 }
 
 func (ck *Clerk) Join(servers map[int][]string) {
-	args := &JoinArgs{}
+	args := &JoinArgs{
+		ClientId:  ck.id,
+		RequestId: nrand(),
+	}
 	// Your code here.
 	args.Servers = servers
-
+	index := ck.leader
 	for {
-		// try each known server.
-		for _, srv := range ck.servers {
+		for i := 0; i < ck.n; i++ {
 			var reply JoinReply
-			ok := srv.Call("ShardMaster.Join", args, &reply)
-			if ok && reply.WrongLeader == false {
+			ok := ck.servers[index].Call("ShardMaster.Join", args, &reply)
+			if ok && reply.WrongLeader == false && reply.Err == OK {
+				ck.leader = index
 				return
+			} else {
+				index = (index + 1) % ck.n
 			}
 		}
 		time.Sleep(100 * time.Millisecond)
@@ -64,17 +78,22 @@ func (ck *Clerk) Join(servers map[int][]string) {
 }
 
 func (ck *Clerk) Leave(gids []int) {
-	args := &LeaveArgs{}
+	args := &LeaveArgs{
+		ClientId:  ck.id,
+		RequestId: nrand(),
+	}
 	// Your code here.
 	args.GIDs = gids
-
+	index := ck.leader
 	for {
-		// try each known server.
-		for _, srv := range ck.servers {
+		for i := 0; i < ck.n; i++ {
 			var reply LeaveReply
-			ok := srv.Call("ShardMaster.Leave", args, &reply)
-			if ok && reply.WrongLeader == false {
+			ok := ck.servers[index].Call("ShardMaster.Leave", args, &reply)
+			if ok && reply.WrongLeader == false && reply.Err == OK {
+				ck.leader = index
 				return
+			} else {
+				index = (index + 1) % ck.n
 			}
 		}
 		time.Sleep(100 * time.Millisecond)
@@ -82,18 +101,24 @@ func (ck *Clerk) Leave(gids []int) {
 }
 
 func (ck *Clerk) Move(shard int, gid int) {
-	args := &MoveArgs{}
+	args := &MoveArgs{
+		ClientId:  ck.id,
+		RequestId: nrand(),
+	}
 	// Your code here.
 	args.Shard = shard
 	args.GID = gid
-
+	index := ck.leader
 	for {
 		// try each known server.
-		for _, srv := range ck.servers {
+		for i := 0; i < ck.n; i++ {
 			var reply MoveReply
-			ok := srv.Call("ShardMaster.Move", args, &reply)
-			if ok && reply.WrongLeader == false {
+			ok := ck.servers[i].Call("ShardMaster.Move", args, &reply)
+			if ok && reply.WrongLeader == false && reply.Err == OK {
+				ck.leader = index
 				return
+			} else {
+				index = (index + 1) % ck.n
 			}
 		}
 		time.Sleep(100 * time.Millisecond)
--- src/shardmaster/server.go
+++ src/shardmaster/server.go
@@ -1,11 +1,42 @@
 package shardmaster
 
+import (
+	"context"
+	"fmt"
+	"labgob"
+	"labrpc"
+	"log"
+	"os"
+	"raft"
+	"sync"
+	"time"
+)
 
-import "raft"
-import "labrpc"
-import "sync"
-import "labgob"
+var Debug = "0"
 
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	if debug := os.Getenv("masterdebug"); len(debug) > 0 {
+		Debug = debug
+	}
+}
+
+func DPrintf(ctx context.Context, format string, a ...interface{}) (n int, err error) {
+	if Debug != "0" {
+		log.Printf("%s, node: %d, log: %s", ctx.Value("type"), ctx.Value("node"), fmt.Sprintf(format, a...))
+	}
+	return
+}
+
+type OpCommand int
+
+const (
+	OpCommandNone = iota
+	OpCommandJoin
+	OpCommandLeave
+	OpCommandMove
+	OpCommandQuery
+)
 
 type ShardMaster struct {
 	mu      sync.Mutex
@@ -14,32 +45,96 @@ type ShardMaster struct {
 	applyCh chan raft.ApplyMsg
 
 	// Your data here.
+	acks         map[int64]int64
+	index        int
+	resultCh     map[int]chan result
+	serverKilled chan bool
 
 	configs []Config // indexed by config num
 }
 
+type result struct {
+	Term  int
+	Value interface{}
+}
 
 type Op struct {
 	// Your data here.
+	Command OpCommand
+	//Key       string
+	Value     interface{}
+	ClientId  int64
+	RequestId int64
 }
-
 
 func (sm *ShardMaster) Join(args *JoinArgs, reply *JoinReply) {
 	// Your code here.
+	DPrintf(sm.context(), "raft join %v", args.Servers)
+	reply.Err, _ = sm.raft(Op{
+		Command:   OpCommandJoin,
+		Value:     args.Servers,
+		ClientId:  args.ClientId,
+		RequestId: args.RequestId,
+	})
+	if reply.Err == ErrWrongLeader {
+		reply.WrongLeader = true
+	}
+	return
 }
 
 func (sm *ShardMaster) Leave(args *LeaveArgs, reply *LeaveReply) {
 	// Your code here.
+	DPrintf(sm.context(), "raft leave %v", args.GIDs)
+	reply.Err, _ = sm.raft(Op{
+		Command:   OpCommandLeave,
+		Value:     args.GIDs,
+		ClientId:  args.ClientId,
+		RequestId: args.RequestId,
+	})
+	if reply.Err == ErrWrongLeader {
+		reply.WrongLeader = true
+	}
+	return
 }
 
 func (sm *ShardMaster) Move(args *MoveArgs, reply *MoveReply) {
 	// Your code here.
+	value := map[int]int{args.Shard: args.GID}
+	DPrintf(sm.context(), "raft move %v", value)
+	reply.Err, _ = sm.raft(Op{
+		Command:   OpCommandMove,
+		Value:     value,
+		ClientId:  args.ClientId,
+		RequestId: args.RequestId,
+	})
+	if reply.Err == ErrWrongLeader {
+		reply.WrongLeader = true
+	}
+	return
 }
 
 func (sm *ShardMaster) Query(args *QueryArgs, reply *QueryReply) {
 	// Your code here.
-}
+	DPrintf(sm.context(), "raft query %v", args.Num)
+	reply.Err, _ = sm.raft(Op{
+		Command: OpCommandQuery,
+		Value:   args.Num,
+	})
+	if reply.Err == ErrWrongLeader {
+		reply.WrongLeader = true
+		return
+	}
 
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	if args.Num == -1 {
+		reply.Config = sm.configs[len(sm.configs)-1]
+	} else if len(sm.configs) > args.Num {
+		reply.Config = sm.configs[args.Num]
+	}
+	DPrintf(sm.context(), "get config %v", reply.Config)
+	return
+}
 
 //
 // the tester calls Kill() when a ShardMaster instance won't
@@ -50,6 +145,9 @@ func (sm *ShardMaster) Query(args *QueryArgs, reply *QueryReply) {
 func (sm *ShardMaster) Kill() {
 	sm.rf.Kill()
 	// Your code here, if desired.
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	close(sm.serverKilled)
 }
 
 // needed by shardkv tester
@@ -71,10 +169,184 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister)
 	sm.configs[0].Groups = map[int][]string{}
 
 	labgob.Register(Op{})
+	labgob.Register(map[int][]string{})
+	labgob.Register([]int{})
+	labgob.Register(map[int]int{})
+
 	sm.applyCh = make(chan raft.ApplyMsg)
-	sm.rf = raft.Make(servers, me, persister, sm.applyCh)
 
 	// Your code here.
+	sm.acks = make(map[int64]int64)
+	sm.resultCh = make(map[int]chan result)
+	sm.serverKilled = make(chan bool)
+
+	go sm.readApplyCh()
+	sm.rf = raft.Make(servers, me, persister, sm.applyCh)
+
+	DPrintf(sm.context(), "start shardmaster")
 
 	return sm
+}
+
+func (sm *ShardMaster) context() context.Context {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "node", sm.me)
+	ctx = context.WithValue(ctx, "type", "server")
+	return ctx
+}
+
+func (sm *ShardMaster) raft(command interface{}) (Err, interface{}) {
+	index, term, ok := sm.rf.Start(command)
+	if !ok {
+		return ErrWrongLeader, ""
+	}
+
+	sm.mu.Lock()
+	ch := make(chan result, 1) // make the send never block
+	// the chan for this index might be overwrite by other requests, so this chan may never receive any message
+	// we close this chan for `id := <=ch` to proceed
+	sm.resultCh[index] = ch
+	sm.mu.Unlock()
+
+	select {
+	case <-sm.serverKilled:
+		return ErrWrongLeader, ""
+	case r := <-ch:
+		if r.Term != term {
+			return ErrWrongLeader, ""
+		}
+		return OK, r.Value
+	case <-time.After(time.Second):
+		sm.mu.Lock()
+		delete(sm.resultCh, index)
+		sm.mu.Unlock()
+		return ErrTimeout, ""
+	}
+}
+
+func (sm *ShardMaster) readApplyCh() {
+	for {
+		select {
+		case <-sm.serverKilled:
+			DPrintf(sm.context(), "server killed")
+			return
+		case msg := <-sm.applyCh:
+			sm.mu.Lock()
+			if !msg.CommandValid {
+				sm.mu.Unlock()
+				break
+			}
+
+			sm.handleKVCommand(msg)
+			sm.mu.Unlock()
+		}
+	}
+}
+
+func (sm *ShardMaster) handleKVCommand(msg raft.ApplyMsg) {
+	// validate msg
+	if msg.CommandIndex <= sm.index {
+		// ignore old messages
+		DPrintf(sm.context(), "get old msg %v", msg.CommandIndex)
+		return
+	}
+	if msg.CommandIndex != sm.index+1 {
+		errMsg := fmt.Sprintf("server %d index %d get command index %d", sm.me, sm.index, msg.CommandIndex)
+		DPrintf(sm.context(), errMsg)
+		panic(errMsg)
+	}
+	sm.index = msg.CommandIndex
+
+	// handle cmd
+	op, _ := msg.Command.(Op)
+	if op.Command != OpCommandQuery {
+		if sm.acks[op.ClientId] != op.RequestId {
+			DPrintf(sm.context(), "get apply msg %v", msg.CommandIndex)
+			// filter old message
+			switch op.Command {
+			case OpCommandJoin:
+				gids := op.Value.(map[int][]string)
+				// copy old
+				conf := sm.configs[len(sm.configs)-1]
+				var newConf Config
+				newConf.Num = conf.Num + 1
+				newConf.Groups = make(map[int][]string, len(conf.Groups)+len(gids))
+				for k, v := range conf.Groups {
+					newConf.Groups[k] = v
+				}
+
+				// generate new
+				for gid, server := range gids {
+					newConf.Groups[gid] = server
+				}
+				newConf.Shards = sm.generateShards(newConf.Groups)
+				sm.configs = append(sm.configs, newConf)
+			case OpCommandLeave:
+				gids := op.Value.([]int)
+				conf := sm.configs[len(sm.configs)-1]
+				var newConf Config
+				newConf.Num = conf.Num + 1
+				newConf.Groups = make(map[int][]string, len(conf.Groups))
+				for k, v := range conf.Groups {
+					newConf.Groups[k] = v
+				}
+				for _, gid := range gids {
+					if _, ok := conf.Groups[gid]; ok {
+						delete(newConf.Groups, gid)
+					}
+				}
+				newConf.Shards = sm.generateShards(newConf.Groups)
+				sm.configs = append(sm.configs, newConf)
+			case OpCommandMove:
+				conf := sm.configs[len(sm.configs)-1]
+				newConf := Config{Num: conf.Num + 1}
+				newConf.Groups = make(map[int][]string, len(conf.Groups))
+				for k, v := range conf.Groups {
+					newConf.Groups[k] = v
+				}
+				for k, v := range conf.Shards {
+					newConf.Shards[k] = v
+				}
+				shardToGid := op.Value.(map[int]int)
+				for shard, gid := range shardToGid {
+					if newConf.Shards[shard] != gid {
+						newConf.Shards[shard] = gid
+					}
+				}
+				sm.configs = append(sm.configs, newConf)
+			default:
+				// do nothing
+			}
+		}
+		sm.acks[op.ClientId] = op.RequestId
+	}
+	// notify new result
+	if ch, ok := sm.resultCh[sm.index]; ok {
+		delete(sm.resultCh, sm.index)
+		select {
+		case <-sm.serverKilled:
+			return
+		case ch <- result{Term: msg.CommandTerm}:
+			DPrintf(sm.context(), "notify result ch index %v success", sm.index)
+		case <-time.After(time.Millisecond * 30):
+			DPrintf(sm.context(), "abandon result ch index %d", sm.index)
+		}
+	}
+}
+
+func (sm *ShardMaster) generateShards(groups map[int][]string) [NShards]int {
+	var r [NShards]int
+	ng := len(groups)
+	if ng == 0 {
+		return r
+	}
+
+	gids := make([]int, 0, len(groups))
+	for k := range groups {
+		gids = append(gids, k)
+	}
+	for i := 0; i < NShards; i++ {
+		r[i] = gids[i%len(groups)]
+	}
+	return r
 }
